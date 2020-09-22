fbdbfb726766b1b1a40a80ee9437e5fa7c2f38d8
premkumarreddy18 <71363033+premkumarreddy18@users.noreply.github.com>
2020-09-22 17:08:51 -0400

    Add files via upload

---
+++ Lab1.sql
@@ -0,0 +1,2 @@
+select film.film_id, film.title, filmcategory.category_id, category.name from sakila.film film join sakila.film_category filmcategory on film.film_id =filmcategory.film_id
+join sakila.category category  on filmcategory.category_id=category.category_id
